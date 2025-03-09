e91e3c5fb34349f009e0fd7517b273215c7b438a
Uvamba <adrune123@gmail.com>
2025-03-09 23:21:22 +0900

    feat: upload TODO task

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import UI from "./ui";
+export default function Home() {
+  return (
+    <main>
+      TODO LIST
+      <UI />
+      <i className="fas fa-check"></i>
+    </main>
+  );
+}
---
+++ components/todo.tsx
@@ -0,0 +1,103 @@
+"use client";
+import {
+  Checkbox,
+  IconButton,
+  Spinner,
+  Tooltip,
+} from "@material-tailwind/react";
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { updateTodo, deleteTodo } from "actions/todoActions";
+import { queryClient } from "config/ReactQueryClientProvider";
+
+export default function Todo({ todo }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [completed, setCompleted] = useState(todo.completed);
+  const [title, setTitle] = useState(todo.title);
+  const updateTodoMutation = useMutation({
+    mutationFn: () =>
+      updateTodo({
+        id: todo.id,
+        title,
+        completed,
+      }),
+    onSuccess: () => {
+      setIsEditing(false);
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+  });
+
+  const deleteTodoMutation = useMutation({
+    mutationFn: () => deleteTodo(todo.id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["todos"],
+      });
+    },
+  });
+
+  const saveCreatedAt = (dateString) => {
+    if (!dateString) return "";
+    const date = new Date(dateString);
+    return date.toLocaleDateString("ko-KR", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    });
+  };
+
+  return (
+    <div className="w-full flex flex-col">
+      <div className="w-full flex items-center gap-1">
+        <Checkbox
+          checked={completed}
+          onChange={async (e) => {
+            await setCompleted(e.target.checked);
+            await updateTodoMutation.mutate();
+          }}
+        />
+        {isEditing ? (
+          <input
+            className="flex-1 border-b-black border-b pb-1"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        ) : (
+          <p className={`flex-1 ${completed && "line-through"}`}>{title}</p>
+        )}
+        {isEditing ? (
+          <IconButton
+            onClick={async () => {
+              await updateTodoMutation.mutate();
+            }}
+          >
+            {updateTodoMutation.isPending ? (
+              <Spinner />
+            ) : (
+              <i className="fas fa-check" />
+            )}
+          </IconButton>
+        ) : (
+          <IconButton onClick={() => setIsEditing(true)}>
+            <i className="fas fa-pen" />
+          </IconButton>
+        )}
+        <IconButton onClick={() => deleteTodoMutation.mutate()}>
+          {deleteTodoMutation.isPending ? (
+            <Spinner />
+          ) : (
+            <i className="fas fa-trash" />
+          )}
+        </IconButton>
+      </div>
+      {todo.created_at && (
+        <div className="text-xs text-black">
+          생성일: {saveCreatedAt(todo.created_at)}
+        </div>
+      )}
+    </div>
+  );
+}
